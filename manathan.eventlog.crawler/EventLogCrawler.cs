377898867c4b42ccc727938ae16cc10fd2d3f77f
manathan-software-GmbH <developer@manathan.net>
2013-10-06 21:37:05 +0200

    Added first draft of crawler for event logs

---
+++ manathan.eventlog.crawler/EventLogCrawler.cs
@@ -0,0 +1,49 @@
+﻿namespace manathan.eventlog.crawler
+{
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using find;
+    using find.Configuration;
+    using find.Crawler;
+
+    public class EventLogCrawler : ICrawler
+    {
+        static IndexedPages _searchConfig;
+
+        public EventLogCrawler()
+        {
+            _searchConfig = IndexedPages.GetConfigSettings();
+        }
+
+        public virtual string CrawlerName
+        {
+            get { return "EventLogCrawler"; }
+        }
+
+        public void Crawl()
+        {
+            _searchConfig.GetPagesForCrawler(CrawlerName).ForEach(CrawlEventLog);
+        }
+
+        void CrawlEventLog(Page page)
+        {
+            var url = page.Url;
+            var log = "application";
+            var machineName = ".";
+            if (page.Options.HasKey("log"))
+            {
+                log = page.Options["log"].Value;
+            }
+
+            if (page.Options.HasKey("machine"))
+            {
+                machineName = page.Options["machine"].Value;
+            }
+
+            var eventLog = page.Options.HasKey("source") ? new EventLog(log, machineName, page.Options["source"].Value) : new EventLog(log, machineName);
+            
+            Parallel.ForEach(eventLog.Entries.Cast<EventLogEntry>(), entry => SearchEngine.AddDocument(new LogDocument(entry), page));
+        }
+    }
+}
---
+++ manathan.eventlog.crawler/LogDocument.cs
@@ -0,0 +1,25 @@
+﻿namespace manathan.eventlog.crawler
+{
+    using System.Diagnostics;
+    using find.Crawler;
+
+    public class LogDocument : BaseDocument
+    {
+        readonly string full;
+
+        public LogDocument(EventLogEntry entry)
+        {
+            Title = entry.Source;
+            Content = entry.Message;
+            Date = entry.TimeWritten;
+            
+            full =
+                string.Format("[Index]\t{0}\n[EventID]\t{1}\n[TimeWritten]\t{2}\n[MachineName]\t{3}\n[Source]\t{4}\n[UserName]\t{5}\n[Message]\t{6}", entry.Index, entry.InstanceId, entry.TimeWritten, entry.MachineName, entry.Source, entry.UserName, entry.Message);
+        }
+
+        public override string ToSearchable()
+        {
+            return full;
+        }
+    }
+}
